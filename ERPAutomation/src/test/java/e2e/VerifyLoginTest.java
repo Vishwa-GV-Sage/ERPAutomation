0fd60f8eb5be1a812d135838143fc1749775b843
Vishwanath G V <vishwanath.gv@sage.com>
2024-02-09 19:53:10 +0530

    End to End test cases

--- ERPAutomation/src/test/java/e2e/VerifyLoginTest.java
+++ ERPAutomation/src/test/java/e2e/VerifyLoginTest.java
@@ -21,7 +21,7 @@ public class VerifyLoginTest extends Helper {
 		loginPage.logintoInbox(inboxUser, inboxPassword);
 		Thread.sleep(10000);
 		
-		System.out.println("Login to Inbox is Successful");
+		System.out.println("Login to Inbox is Successful and verified");
 	}
 	catch (Exception e) {e.printStackTrace();}
 	}
